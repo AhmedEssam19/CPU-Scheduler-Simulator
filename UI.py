a14a6198667c9d6ecaae15216a28a599c13f5729
Ahmed Essam <ahmed.elhlwgy@gmail.com>
2021-04-24 22:58:09 +0200

    Enhance user experience with input processes

--- UI.py
+++ UI.py
@@ -7,15 +7,18 @@ ALGORITHMS = ['FCFS', 'SJF', 'Priority', 'Round Robin']
 
 state = {
     'row': 0,
-    'BACKGROUND_COLOR': '#2c313c'
+    'BACKGROUND_COLOR': '#2c313c',
+    'processes': [],
+    'processes_labels': []
 }
 
 
 def main():
     root = tk.Tk()
-    root.geometry("1400x600")
+    root.geometry("1450x600")
     root.configure(bg='#2c313c')
 
+    # Make the main frame
     main_frame = Frame(root, bg=state['BACKGROUND_COLOR'])
     main_frame.pack(fill=BOTH, expand=1)
 
@@ -26,27 +29,31 @@ def main():
     algorithm_label = tk.Label(row1, text='Choose Algorithm:', bg=state['BACKGROUND_COLOR'], fg='white')
     algorithm_label.place(relx=0, rely=0, relheight=1, relwidth=0.2)
     algorithm_input = ttk.Combobox(row1, values=ALGORITHMS, justify="center", font=ENTRY_FONT, state="readonly")
-    algorithm_input.bind("<<ComboboxSelected>>", None)
+    algorithm_input.bind("<<ComboboxSelected>>", lambda _: select_algorithm(state, algorithm_input, preemptive_choice,
+                                                                            quantum_input, add_process_button))
     algorithm_input.place(relx=0.2, rely=0.2, relwidth=0.15, relheight=0.6)
 
     # check if it's preemptive
     isPreemptive = tk.BooleanVar()
-    preemptive_choice = tk.Checkbutton(row1, text='Preemptive', variable=isPreemptive, onvalue=True, offvalue=False)
+    state['isPreemptive'] = isPreemptive
+    preemptive_choice = tk.Checkbutton(row1, text='Preemptive', variable=isPreemptive,
+                                       onvalue=True, offvalue=False, state='disable')
     preemptive_choice.place(relx=0.4, rely=0, relheight=1, relwidth=0.15)
 
     # quantum time
     quantum_label = tk.Label(row1, text='Quantum Time:', bg=state['BACKGROUND_COLOR'], fg='white')
     quantum_label.place(relx=0.6, rely=0, relheight=1, relwidth=0.2)
-    quantum_input = tk.Entry(row1, borderwidth=4, justify="center", font=10, state="normal")
+    quantum_input = tk.Entry(row1, borderwidth=4, justify="center", font=10, state="disable")
     quantum_input.place(relx=0.8, rely=0.0, relheight=1, relwidth=0.1)
 
-    second_frame = Frame(main_frame, bd=10, bg='white')
-    second_frame.place(relx=0, rely=0.1, relwidth=1, relheight=0.6)
+    # Make frame for input processes
+    processes_input_frame = Frame(main_frame, bd=10, bg='white')
+    processes_input_frame.place(relx=0, rely=0.1, relwidth=1, relheight=0.6)
 
-    my_canvas = Canvas(second_frame)
+    my_canvas = Canvas(processes_input_frame)
     my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
 
-    my_scrollbar = ttk.Scrollbar(second_frame, orient=VERTICAL, command=my_canvas.yview)
+    my_scrollbar = ttk.Scrollbar(processes_input_frame, orient=VERTICAL, command=my_canvas.yview)
     my_scrollbar.pack(side=RIGHT, fill=Y)
 
     my_canvas.configure(yscrollcommand=my_scrollbar.set)
@@ -57,7 +64,9 @@ def main():
 
     my_canvas.create_window((0, 0), window=inner_frame, anchor='nw')
 
-    add_process_button = tk.Button(main_frame, text="Add Process", command=lambda: add_process(inner_frame, state))
+    # Make button to add processes
+    add_process_button = tk.Button(main_frame, text="Add Process",
+                                   command=lambda: add_process(inner_frame, state, algorithm_input), state='disable')
     add_process_button.place(rely=0.71, relx=0.84, relwidth=0.15, relheight=0.1)
 
     root.mainloop()
--- gui_utils.py
+++ gui_utils.py
@@ -1,21 +1,72 @@
 import tkinter as tk
 
+from collections import namedtuple
+
+Process = namedtuple('Process', 'task_name arrival_time burst_time priority')
+
 
 def reset_scroll_region(canvas):
     canvas.configure(scrollregion=canvas.bbox("all"))
 
 
-def add_process(frame, state):
-    label = tk.Label(frame, text='Task Name', bg=state['BACKGROUND_COLOR'], fg='white')
-    label.grid(row=state['row'], column=0, padx=10)
-    entry = tk.Entry(frame, borderwidth=4, justify="center", font=10, state="normal")
-    entry.grid(row=state['row'], column=1, padx=10, )
-    label2 = tk.Label(frame, text='Arrival Time', bg=state['BACKGROUND_COLOR'], fg='white')
-    label2.grid(row=state['row'], column=2, padx=10)
-    entry2 = tk.Entry(frame, borderwidth=4, justify="center", font=10, state="normal")
-    entry2.grid(row=state['row'], column=3, padx=10)
-    label3 = tk.Label(frame, text='Burst Time', bg=state['BACKGROUND_COLOR'], fg='white')
-    label3.grid(row=state['row'], column=4, padx=10)
-    entry3 = tk.Entry(frame, borderwidth=4, justify="center", font=10, state="normal")
-    entry3.grid(row=state['row'], column=5, padx=10)
+def add_process(frame, state, algorithms_combobox):
+    task_name_label = tk.Label(frame, text='Task Name', bg=state['BACKGROUND_COLOR'], fg='white')
+    task_name_label.grid(row=state['row'], column=0, padx=10)
+    task_name_entry = tk.Entry(frame, borderwidth=4, justify="center", font=10)
+    task_name_entry.grid(row=state['row'], column=1, padx=10, )
+
+    arrival_time_label = tk.Label(frame, text='Arrival Time', bg=state['BACKGROUND_COLOR'], fg='white')
+    arrival_time_label.grid(row=state['row'], column=2, padx=10)
+    arrival_time_entry = tk.Entry(frame, borderwidth=4, justify="center", font=10)
+    arrival_time_entry.grid(row=state['row'], column=3, padx=10)
+
+    burst_time_label = tk.Label(frame, text='Burst Time', bg=state['BACKGROUND_COLOR'], fg='white')
+    burst_time_label.grid(row=state['row'], column=4, padx=10)
+    burst_time_entry = tk.Entry(frame, borderwidth=4, justify="center", font=10)
+    burst_time_entry.grid(row=state['row'], column=5, padx=10)
+
+    algorithm = algorithms_combobox.get()
+    priority_entry = None
+    priority_label = None
+    if algorithm == 'Priority':
+        priority_label = tk.Label(frame, text='Priority', bg=state['BACKGROUND_COLOR'], fg='white')
+        priority_label.grid(row=state['row'], column=6, padx=10)
+        priority_entry = tk.Entry(frame, borderwidth=4, justify="center", font=10)
+        priority_entry.grid(row=state['row'], column=7, padx=10)
+
+    new_process = Process(task_name_entry, arrival_time_entry, burst_time_entry, priority_entry)
+    state['processes'].append(new_process)
+    state['processes_labels'].append([task_name_label, arrival_time_label, burst_time_label, priority_label])
     state['row'] += 1
+
+
+def select_algorithm(state, algorithms_combobox, preemptive_checkbox, quantum_input, add_process_button):
+    clear_processes(state)
+    algorithm = algorithms_combobox.get()
+
+    add_process_button['state'] = 'normal'
+    preemptive_checkbox['state'] = 'disable'
+    quantum_input['state'] = 'disable'
+
+    if algorithm == 'SJF' or algorithm == 'Priority':
+        preemptive_checkbox['state'] = 'normal'
+
+    elif algorithm == 'Round Robin':
+        quantum_input['state'] = 'normal'
+
+
+def clear_processes(state):
+    for process in state['processes']:
+        process.task_name.destroy()
+        process.arrival_time.destroy()
+        process.burst_time.destroy()
+
+        if process.priority is not None:
+            process.priority.destroy()
+
+    for process_labels in state['processes_labels']:
+        for label in process_labels:
+            if label is not None:
+                label.destroy()
+
+    state['row'] = 0
